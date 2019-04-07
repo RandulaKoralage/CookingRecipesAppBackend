6bad88f99bb42fcf09c1248038011b217349f558
Randula_u <randulakoralage82@gmail.com>
2019-04-07 16:02:50 +0530

    searchbyname

--- cookingapp-api/server.js
+++ cookingapp-api/server.js
@@ -15,7 +15,7 @@ app.use(jwt());
 
 // api routes
 app.use('/users', require('./users/users.controller'));
-
+app.use('/recipes', require('./recipes/recipes.controller'));
 // global error handler
 app.use(errorHandler);
 
