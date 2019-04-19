0add6c7b228880bac8d262e61ef64d7f2ca2d55a
Randula_u <randulakoralage82@gmail.com>
2019-04-19 21:37:19 +0530

    ingredient base search

--- cookingapp-api/recommendation/recommendation.controller.js
+++ cookingapp-api/recommendation/recommendation.controller.js
@@ -8,7 +8,7 @@ module.exports = router;
 
 //looking for recipes with relative ingredients
 function recommondsimiler(req, res, next) {
-    console.log(req.body.id);
+   // console.log(req.body.id);
     recommondationService.recommondsimiler(req.body.id)
         .then(recipe => res.json(recipe))
         .catch(err => next(err));
