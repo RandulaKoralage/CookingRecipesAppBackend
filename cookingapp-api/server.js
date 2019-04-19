1f7371528eadc02c08064e954cce2c7807db8903
Randula_u <randulakoralage82@gmail.com>
2019-04-19 19:59:46 +0530

    recommondation based on ingredients

--- cookingapp-api/server.js
+++ cookingapp-api/server.js
@@ -16,6 +16,7 @@ app.use(jwt());
 // api routes
 app.use('/users', require('./users/users.controller'));
 app.use('/recipes', require('./recipes/recipes.controller'));
+app.use('/recommend', require('./recommendation/recommendation.controller'));
 // global error handler
 app.use(errorHandler);
 
